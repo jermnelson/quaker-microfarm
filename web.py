a3d655031e5d779ad4d79db17a0f8cabb35d0118
Jeremy Nelson <jermnelson.gmail.com>
2014-06-06 17:09:16 -0600

    added bed1 and bed2 for 2014

--- web.py
+++ web.py
@@ -1,10 +1,15 @@
+import json
 from flask import abort, Flask, render_template
+
 
 farm = Flask('farm')
 
 @farm.route('/')
 def default():
-    return render_template('index.html')
+    beds = [json.load(open("data/2014/bed-one.json")),
+            json.load(open("data/2014/bed-two.json"))]
+    return render_template('index.html',
+                           beds=beds)
 
 if __name__ == '__main__':
     farm.run(
